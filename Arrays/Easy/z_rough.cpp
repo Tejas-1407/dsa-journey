cb2c4d0275e6265d844f960df7617e78394b90c4
Tejas-1407 <tejasbatav014@gmail.com>
2026-07-19 13:02:51 +0530

    19-7-26

--- Arrays/Easy/z_rough.cpp
+++ Arrays/Easy/z_rough.cpp
@@ -4,21 +4,19 @@
 #include <climits>
 using namespace std ;
 
-void CS (vector <int > &v){
+bool CS (vector <int > &v){
     for (int i = 0 ; i < v.size() -1 ;i++){
         if (v[i] > v[i+1]){
-            cout << "not sorted " ;
-            return ;
+            return false;
         }
     }
-    cout << "sorted" ;
-    return ;
+    return true;
 }
 
 int main (){
 
     vector <int > A = {1,2 ,2,3,3,3,4,6,5} ;
-    CS (A) ;
+    cout << CS (A) ;
     cout <<endl ; 
     for (auto it : A){
         cout << it << endl;
