22bbc5d04153746fae5cc73ed676f7ef55de77a0
Tejas-1407 <tejasbatav014@gmail.com>
2026-07-19 12:57:19 +0530

    19-7-26

--- Arrays/Easy/z_rough.cpp
+++ Arrays/Easy/z_rough.cpp
@@ -4,25 +4,22 @@
 #include <climits>
 using namespace std ;
 
-int RD (vector <int > &v){
-    set <int> s ;
-    for (int i = 0 ;i < v.size (); i++){
-        s.insert (v[i]) ;
+int ORD (vector <int > &v){
+    int i = 0 ; 
+    for (int j = 0 ; j < v.size() ;j++){
+        if (v[i] != v[j]){
+            i++ ;
+            v[i] = v[j] ;
+        }
     }
-    int idx = 0 ;
-    for (auto it : s){
-        v[idx] = it ;
-        idx++ ;
-    }
-    v.resize(idx);
-    return idx ;
+    v.resize(i+1);
+    return i+1 ;
 }
-
 
 int main (){
 
     vector <int > A = {1,2 ,2,3,3,3,4,5} ;
-    cout << RD (A) <<endl ; 
+    cout << ORD (A) <<endl ; 
     for (auto it : A){
         cout << it << endl;
     }
