ea5278fe5f32411676bf646a94b523abe7784875
Tejas-1407 <tejasbatav014@gmail.com>
2026-07-31 14:38:18 +0530

    31-07-26 , sem 5 day 1

--- Arrays/Easy/07_Right_rotate.cpp
+++ Arrays/Easy/07_Right_rotate.cpp
@@ -1,1 +1,28 @@
 //right rotation by D places Assignment
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <climits>
+#include <algorithm>
+using namespace std ;
+
+
+void RR(vector<int> &v, int D) {
+    if (v.size() == 0) return;
+    D = D % v.size();
+    reverse(v.begin() + v.size() - D, v.end());   // Step 1: reverse last D elements
+    reverse(v.begin(), v.begin() + v.size() - D); // Step 2: reverse first n-D elements
+    reverse(v.begin(), v.end());                  // Step 3: reverse whole vector
+}
+
+
+int main (){
+
+    vector <int > v  = { 1,2,3,3,4,4,5,5,6,7,7,7};
+    RR(v , 3) ;
+    for (auto it : v){
+        cout << it << endl ;
+    }
+    return  0 ;
+}
--- Arrays/Easy/z_rough.cpp
+++ Arrays/Easy/z_rough.cpp
@@ -5,31 +5,17 @@
 #include <algorithm>
 using namespace std ;
 
-void LR(vector <int > &v , int D){
-    int n = D % v.size ();
-    if (v.size() == 0 ) return ;
-    vector <int> temp ;
-    for (int i = 0 ; i < n ; i++){
-        temp.push_back(v[i]) ;
-    }
-    for (int i = n ; i < v.size() ; i++){
-        v[i-n] = v[i] ;
-    }
-    for (int i = v.size() - n ;i < v.size() ; i++){
-        v[i] = temp [i - (v.size()- n)] ;
-    }
-}
-void OLR (vector <int > & v , int D){
-    if (v.size() == 0 ) return ;
+void RR (vector <int > &v , int D) {
+    if (v.size()== 0 )return ;
     D = D % v.size() ;
-    reverse (v.begin() ,v.begin () + D) ;
-    reverse (v.begin() + D,v.begin () + v.size()) ;
-    reverse (v.begin() ,v.begin () + v.size()) ;
+    reverse (v.begin() + v.size()- D , v.end() ) ;
+    reverse (v.begin()  , v.begin() + v.size() -D ) ;
+    reverse (v.begin() , v.end() ) ;
 }
 int main (){
 
     vector <int > v  = { 1,2,3,3,4,4,5,5,6,7,7,7};
-    OLR(v , 3) ;
+    RR(v , 3) ;
     for (auto it : v){
         cout << it << endl ;
     }
