baeadc8cb535cd8d99ab49ca36e2944097c22368
Tejas-1407 <tejasbatav014@gmail.com>
2026-08-01 00:50:41 +0530

    31-07-26 , sem 5 day 1

--- Arrays/Easy/12_sorting.cpp
+++ Arrays/Easy/12_sorting.cpp
@@ -17,13 +17,39 @@ void bubble_sort(vector <int> &v){
     }
 }
 
+// selection sort [__sorted__ |__unsorted__ ]
+void selection_sort (vector <int > &v){
+    for (int i = 0 ; i < v.size() - 1 ;i++){
+        int smallest = i ; 
+        for (int j = i + 1 ; j < v.size () ; j++){
+            if (v [j] < v[smallest]){
+                smallest = j  ;
+            }
+        }
+        swap (v[i] ,  v[smallest]) ;
+    }
+}
+
+
+void insertion_sort(vector <int> &v) {
+    if (v.size() == 0) return ;
+    for(int i = 1 ; i < v.size() ;i++){
+        int curr = v[i] ;
+        int prev = i - 1  ;
+        while ( prev >= 0 && v[prev] > curr){
+            v[prev + 1] = v [prev] ;
+            prev -- ;
+        } 
+        v[prev + 1] = curr ; // prev + 1 bcz of earlier prev -- 
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     vector <int> v = {1,2,4,5,7,9,0,6};
-    bubble_sort(v) ;
+    insertion_sort(v) ;
     for (auto it : v){
         cout << it << endl ;
     }
