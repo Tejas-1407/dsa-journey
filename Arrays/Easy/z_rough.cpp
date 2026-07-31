0cfb0cf4720ff04148d2ec2c8f69d58004174623
Tejas-1407 <tejasbatav014@gmail.com>
2026-07-31 14:24:25 +0530

    31-07-26 , sem 5 day 1

--- Arrays/Easy/z_rough.cpp
+++ Arrays/Easy/z_rough.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <set>
 #include <climits>
+#include <algorithm>
 using namespace std ;
 
 void LR(vector <int > &v , int D){
@@ -18,11 +19,18 @@ void LR(vector <int > &v , int D){
         v[i] = temp [i - (v.size()- n)] ;
     }
 }
+void OLR (vector <int > & v , int D){
+    if (v.size() == 0 ) return ;
+    D = D % v.size() ;
+    reverse (v.begin() ,v.begin () + D) ;
+    reverse (v.begin() + D,v.begin () + v.size()) ;
+    reverse (v.begin() ,v.begin () + v.size()) ;
+}
 int main (){
 
-    vector <int>v ={ 1,2,3,3,4,4,5,5,6,7,7,7};
-    LR (v , 3) ;
-    for (auto it : v ){
+    vector <int > v  = { 1,2,3,3,4,4,5,5,6,7,7,7};
+    OLR(v , 3) ;
+    for (auto it : v){
         cout << it << endl ;
     }
     return  0 ;
