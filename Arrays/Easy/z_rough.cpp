6cfb5aac847f73203f8358ecb81980c46d627604
Tejas-1407 <tejasbatav014@gmail.com>
2026-07-31 10:39:06 +0530

    31-07-26 , sem 5 day 1

--- Arrays/Easy/z_rough.cpp
+++ Arrays/Easy/z_rough.cpp
@@ -4,29 +4,26 @@
 #include <climits>
 using namespace std ;
 
-void LR (vector <int > & v){
-    for (int i = 0 ; i < v.size() -1 ;i++){
-        int temp = v[i] ;
-        v[i] =  v [i+1] ;
-        v[i+1 ] = temp  ;
+void LR(vector <int > &v , int D){
+    int n = D % v.size ();
+    if (v.size() == 0 ) return ;
+    vector <int> temp ;
+    for (int i = 0 ; i < n ; i++){
+        temp.push_back(v[i]) ;
+    }
+    for (int i = n ; i < v.size() ; i++){
+        v[i-n] = v[i] ;
+    }
+    for (int i = v.size() - n ;i < v.size() ; i++){
+        v[i] = temp [i - (v.size()- n)] ;
     }
 }
-
-void LRD (vector <int > &v, int D ){
-    int n = D% v.size() ;
-    for (int i = n -1  ; i < v.size ()-n ;i++ ){
-        v[i]
-    }
-}
-
-
 int main (){
 
-    vector <int > A = {1,2 ,2,3,3,3,4,6,5} ;
-     LR (A) ;
-    cout <<endl ; 
-    for (auto it : A){
-        cout << it << endl;
+    vector <int>v ={ 1,2,3,3,4,4,5,5,6,7,7,7};
+    LR (v , 3) ;
+    for (auto it : v ){
+        cout << it << endl ;
     }
     return  0 ;
 }
