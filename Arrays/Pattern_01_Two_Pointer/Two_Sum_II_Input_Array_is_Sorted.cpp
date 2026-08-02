1b46a238a16774ef70c01f2ad0f6960472d8680b
Tejas-1407 <tejasbatav014@gmail.com>
2026-08-02 13:55:49 +0530

    02-08-26 , sem 5 day 3

---
+++ Arrays/Pattern_01_Two_Pointer/Two_Sum_II_Input_Array_is_Sorted.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+//given increasing number array so v[n-1] > v[0]
+
+vector <int> twosum (vector <int> &v , int target){
+    int i = 0 ; 
+    int j = v.size() - 1 ;
+    while ( i < j ){
+        int sum = v[i] + v[j] ;
+        if (sum == target ){
+            return {i+1 , j+1}; 
+        }
+        else if (sum > target){
+            j-- ;
+        }
+        else {
+            i++ ;
+        }
+    }
+    return {} ;
+}
+
+int main () {
+
+    //code 
+    vector <int> v = {2,7,11,15} ;
+    vector <int> solution = twosum(v , 9) ;
+
+    for (auto it  : solution){
+        cout << it << endl ;
+    }
+
+    return 0 ;
+}
