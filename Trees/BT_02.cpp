b2ab707af8ac19d76a88cb100b54467fe2efd6c2
numb_vector <tejasbatav014@gmail.com>
2026-08-21 00:04:39 +0530

    Day 21 , sem 5 , 20-08-2026 , I am sorry ...

--- Trees/BT_02.cpp
+++ Trees/BT_02.cpp
@@ -31,11 +31,47 @@ Node * BuildTree (vector <int> &v ) {
     return root ;
 }
 
+
+void preorder (Node * root){
+
+    if (root == NULL) {
+        return  ;
+    }
+    
+    cout  << root -> data  << " -> " ;
+    preorder (root->left) ;
+    preorder (root->right) ;
+
+}
+void inorder (Node * root){
+
+    if (root == NULL) {
+        return  ;
+    }
+    
+    inorder (root->left) ;
+    cout  << root -> data  << " -> " ;
+    inorder (root->right) ;
+
+}
+void postorder (Node * root){
+
+    if (root == NULL) {
+        return  ;
+    }
+    
+    postorder (root->right) ;
+    postorder (root->left) ;
+    cout  << root -> data  << " -> " ;
+
+}
+
 int main() {
     
     // code 
     vector <int> v = {1,2,-1,-1,3,4,-1,-1,5,-1,-1} ;
     Node * root = BuildTree (v) ;
     cout << root ->left -> data << endl ;
+    inorder (root) ;
     return 0;
 }
